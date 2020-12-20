d600ecf250c0965e6a11e6006e8624d237888be1
2718163908 <2718163908@qq.com>
2020-12-20 20:04:58 +0800

    此处写这次提交代码所附带的信息

---
+++ chouquone/src/main/java/com/example/chouquone/model/MainModel.java
@@ -0,0 +1,17 @@
+package com.example.chouquone.model;
+import com.example.mylianxi.contract.MainContract;
+import com.example.mylianxi.net.Callback;
+import com.example.mylianxi.net.RetrofitUtils;
+
+public class MainModel implements MainContract.IMainModel {
+    private MainContract.IMainPresenter presenter;
+
+    public MainModel(MainContract.IMainPresenter presenter) {
+        this.presenter = presenter;
+    }
+
+    @Override
+    public <T> void getLoginData(String url, Callback<T> callback) {
+        RetrofitUtils.getInstance().get(url, callback);
+    }
+}
