463069e13635389dab31789c3bf408862dbd648a
2718163908 <2718163908@qq.com>
2020-12-20 20:09:57 +0800

    此处写这次提交代码所附带的信息

---
+++ chouqutwo/src/main/java/com/example/chouqutwo/api/URLConstant.java
@@ -0,0 +1,6 @@
+package com.example.chouqutwo.api;
+
+public class URLConstant {
+    public static String BASE_URL = "http://cdwan.cn:7000/";
+    public static String NEW_LIST = BASE_URL + "exam2003/anewslist.json";
+}
